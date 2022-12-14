c7b201f646e8da9f2729b08312ac178ae60c9b39
Ekanat33 <88494593+Ekanat33@users.noreply.github.com>
2022-12-14 16:56:53 +0700

    เเก้

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -4,8 +4,8 @@ package entities;
 
 public abstract class Entity {
    protected float x,y;  
-   public Entity(float x, float y){
+   public Entity(float x,float y){
        this.x = x;
        this.y = y;
-   }
+   } 
 }
--- src/main/Game.java
+++ src/main/Game.java
@@ -1,7 +1,7 @@
 package main;
 
 import entities.Player;
-import levels.LevelManager;
+import java.awt.Graphics;
 
 public class Game implements Runnable {
 
@@ -9,61 +9,85 @@ public class Game implements Runnable {
 	private GamePanel gamePanel;
 	private Thread gameThread;
 	private final int FPS_SET = 120;
-	private LevelManager levelManager;
-
-	public static final int TILES_DEFAULT_SIZE = 32;
-	public static final float SCALE = 1.25f;
-	public static final int TILES_IN_WIDTH = 26;
-	public static final int TILES_IN_HEIGHT = 14;
-	public static final int TILES_SIZE = (int)(TILES_DEFAULT_SIZE * SCALE);
-	public static final int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
-	public static final int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
-
+        private final int UPS_SET  = 200;
+        private Player player;
 	public Game() {
-
-		gamePanel = new GamePanel();
+                 initClasses(); 
+		gamePanel = new GamePanel(this);
 		gameWindow = new GameWindow(gamePanel);
 		gamePanel.requestFocus();
+              
 		startGameLoop();
-
+                
 	}
-
-	private void initClasses(){
-		player = new Player(200, 200);
-	}
-
 
 	private void startGameLoop() {
 		gameThread = new Thread(this);
 		gameThread.start();
 	}
+        public void update(){
+            player.update();
+        }
+        public void render(Graphics g){
+            player.render(g);
+        }
 
 	@Override
 	public void run() {
 
 		double timePerFrame = 1000000000.0 / FPS_SET;
-		long lastFrame = System.nanoTime();
-		long now = System.nanoTime();
+                double timePerUpdate = 1000000000.0 / UPS_SET;
+		
+                long previousTime = System.nanoTime();
+                
 
 		int frames = 0;
+                int updates = 0;
+                double deltaF = 0;
+                double deltaU = 0;
 		long lastCheck = System.currentTimeMillis();
 
 		while (true) {
+                        
+                    
+			
+                        long currentTime = System.nanoTime();
+                        
+                        deltaU += (currentTime - previousTime) / timePerUpdate;
+                        deltaF += (currentTime - previousTime) / timePerFrame;
+                        previousTime = currentTime;
+                        if(deltaU >= 1){
+                            update();
+                            updates++;
+                            deltaU--;
+                        }
+                        if(deltaF >=1){
+                             gamePanel.repaint();
+				deltaF--;
+				frames++; 
+                        }
+                        
 
-			now = System.nanoTime();
-			if (now - lastFrame >= timePerFrame) {
-				gamePanel.repaint();
-				lastFrame = now;
-				frames++;
-			}
 
 			if (System.currentTimeMillis() - lastCheck >= 1000) {
 				lastCheck = System.currentTimeMillis();
-				System.out.println("FPS: " + frames);
+				System.out.println("FPS: " + frames + "//UPS: " + updates);
+                                updates = 0;
 				frames = 0;
 			}
 		}
 
 	}
+
+    private void initClasses() {
+        player = new Player(200,200);
+    }
+    public Player getPlayer(){
+        return player;
+    }
+    public void windowFocusLost(){
+        player.resetDirBooleans();
+    }
+   
 
 }
