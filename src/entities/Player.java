66a6fb54b1a77e254bb0c1a7a9647719415e862f
Ekanat33 <88494593+Ekanat33@users.noreply.github.com>
2022-12-15 20:33:13 +0700

    ปรับ hitbox ให้เท่าตัวได้เเล้ว

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -9,6 +9,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.imageio.ImageIO;
+import main.Game;
 
 import utilz.LoadSave;
 
@@ -28,23 +29,26 @@ public class Player extends Entity {
     private boolean left, up, right, down;
     private float playerSpeed = 2.0f;
     private int[][] lvlData;
+    private float xDrawOffset = 64*Game.SCALE;
+    private float yDrawOffset = 23*Game.SCALE;
     private boolean moving = false, attacking = false;
 
    public Player(float x, float y, int width, int height) {
 		super(x, y, width, height);
 		loadAnimations();
-}
+                initHitbox(x, y, 35 * Game.SCALE, 63 * Game.SCALE);
+}   
 
     public void update() {
         updatePos();
-        updateHitbox();
+        
         updateAnimationTick();
         setAnimation();
 
     }
 
     public void render(Graphics g) {
-       g.drawImage(animations[playerAction][aniIndex], (int) x, (int) y, width, height, null);
+       g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset), (int) (hitbox.y-yDrawOffset), width, height, null);
        drawHitbox(g);
     }
     //
@@ -94,9 +98,14 @@ public class Player extends Entity {
 		else if (down && !up)
 			ySpeed = playerSpeed;
 
-		if (CanMoveHere(x + xSpeed, y + ySpeed, width, height, lvlData)) {
-			this.x += xSpeed;
-			this.y += ySpeed;
+//		if (CanMoveHere(x + xSpeed, y + ySpeed, width, height, lvlData)) {
+//			this.x += xSpeed;
+//			this.y += ySpeed;
+//			moving = true;
+//		}
+                if (CanMoveHere(hitbox.x + xSpeed, hitbox.y + ySpeed, hitbox.width, hitbox.height, lvlData)) {
+			hitbox.x += xSpeed;
+			hitbox.y += ySpeed;
 			moving = true;
 		}
 
