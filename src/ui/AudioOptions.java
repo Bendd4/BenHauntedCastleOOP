04948c92141b4559d25e8fa643faaa1ff8b96ebb
AnuwatPun <88495729+AnuwatPun@users.noreply.github.com>
2022-12-20 23:25:53 +0700

    Update AudioOptions.java

--- src/ui/AudioOptions.java
+++ src/ui/AudioOptions.java
@@ -15,7 +15,9 @@ public class AudioOptions {
     private VolumeButton volumeButton;
     private Soundbutton musicButton, sfxButton;
 
-    public AudioOptions() {
+    private Game game;
+    public AudioOptions(Game game) {
+        this.game = game;
         createSoundButtons();
         createVolumeButton();
 
@@ -51,9 +53,13 @@ public class AudioOptions {
         volumeButton.draw(g);
     }
 
-    public void mouseDragged(MouseEvent e) {
+   public void mouseDragged(MouseEvent e) {
         if (volumeButton.isMousePressed()) {
+            float valueBefore = volumeButton.getFloatValue();
             volumeButton.changeX(e.getX());
+            float valueAfter = volumeButton.getFloatValue();
+            if(valueBefore != valueAfter)
+                game.getAudioPlayer().setVolume(valueAfter);
         }
     }
 
@@ -71,10 +77,12 @@ public class AudioOptions {
         if (isIn(e, musicButton)) {
             if (musicButton.isMousePressed()) {
                 musicButton.setMuted(!musicButton.isMuted());
+                game.getAudioPlayer().toggleSongMute();
             }
         } else if (isIn(e, sfxButton)) {
             if (sfxButton.isMousePressed()) {
                 sfxButton.setMuted(!sfxButton.isMuted());
+                game.getAudioPlayer().toggleEffectMute();
             }
         }
 
